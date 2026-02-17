578934230ddffa3971ef8aea2a04ecd3eaf395c8
fsfg <8500077+fsfg@users.noreply.github.com>
2026-02-17 12:41:28 +0200

    Configure Oban

--- mix.exs
+++ mix.exs
@@ -52,7 +52,9 @@ defmodule TaskPipeline.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:uuidv7, "~> 1.0"}
+      {:uuidv7, "~> 1.0"},
+      {:oban, "~> 2.19"},
+      {:igniter, "~> 0.5", only: [:dev]}
     ]
   end
 
